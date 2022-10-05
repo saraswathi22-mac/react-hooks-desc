7b8725f932dd7698f76c1286d29c9391c5819269
exit <saraswathi.mac@gmail.com>
2022-10-05 18:38:33 +0530

    Added useRef hooks

--- src/Components/UseRef.tsx
+++ src/Components/UseRef.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import img from './images/useRef.jpg'
+import img from "./images/useRef.jpg";
 
 const UseRef = () => {
   return (
@@ -16,9 +16,46 @@ const UseRef = () => {
       <li>The returned object has a special property called current.</li>
       <img src={img} alt="use ref" className="img" />
       <li>
-        useRef also never rerenders, which can help keep track of the total
-        keystrokes of a user or the total amount of times a button is clicked.
+        We use the useRef hook to store the values without triggering the
+        re-render of the component.
       </li>
+      <h3>What Did I Use the useRef Hook for?</h3>
+      <li>Handling Form Inputs and Accessing DOM Nodes/Elements</li>
+      <p>
+        Using useRef, we will implement DOM accessing, form input handling, and
+        persisting values across the component re-renders in a single
+        application.
+      </p>
+      <a
+        href="https://stackblitz.com/edit/react-6r9dfh?file=src%2FApp.js"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Click me for Code Example &#9997;
+      </a>
+      <li>Storing Previous State/Value</li>
+      <p>
+        The useRef hook persists values ​​between re-renders. With the help of
+        the useEffect, the state can be stored in a reference before the value
+        changes. This previous value becomes available in the next render via
+        ref.
+      </p>
+      <a
+        href="https://stackblitz.com/edit/react-mypnev?file=src%2FApp.js"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Click me for Code Example &#9997;
+      </a>
+      <li>Timer</li>
+      <p>The useRef is used to store the active timer id.</p>
+      <a
+        href="https://stackblitz.com/edit/react-einhge?file=src%2FApp.js"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Click me for Code Example &#9997;
+      </a>
       <li>
         To pass a ref to a child or parent component forwardRef is needed, as
         ref instances don’t function the same as regular props.
