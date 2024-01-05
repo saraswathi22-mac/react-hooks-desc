42349d970a8c344a8f14eeec1d0487d87d5dee04
exit <saraswathi.mac@gmail.com>
2024-01-05 22:19:39 +0530

    Updated Headings

---
+++ src/components/common/Headings/Headings.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface Title {
+  title: string;
+}
+
+const MainHeading: React.FC<Title> = ({ title }) => {
+  return (
+    <div className="text-5xl font-bold text-center font-serif bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-red-500">
+      {title}
+    </div>
+  );
+};
+
+const HookHeading: React.FC<Title> = ({ title }) => {
+  return <h1 className="heading">{title}</h1>;
+};
+
+export { MainHeading, HookHeading };
